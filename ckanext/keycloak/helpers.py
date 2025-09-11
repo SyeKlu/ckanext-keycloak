7198245b63278405136f1755054402a462442a9f
Sebastian-Sye Klute <sk@teuto.net>
2025-09-11 11:20:19 +0200

    fix(ckanext-keycloak): add group membership ckan dcat group

--- ckanext/keycloak/helpers.py
+++ ckanext/keycloak/helpers.py
@@ -13,6 +13,22 @@ from os import environ
 log = logging.getLogger(__name__)
 
 
+dcat_ap_de_groups = [
+    "agri",
+    "econ",
+    "educ",
+    "ener",
+    "envi",
+    "heal",
+    "intr",
+    "just",
+    "soci",
+    "gove",
+    "regi",
+    "tech",
+    "tran",
+]
+
 def generate_password():
     alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for _ in range(8))
@@ -59,6 +75,67 @@ def activate_user_if_deleted(user):
         user.commit()
         log.info(u'User {} reactivated'.format(user.name))
 
+def handle_group_memberships(user_dict):
+    tenants = {group.split('.')[0] for group in user_dict['groups']}
+    tenant_ids = {hash_string_to_uuid(tenant): tenant for tenant in tenants}
+
+    current_ckan_tenants = _current_orgs()
+    current_ckan_tenants_id = {hash_string_to_uuid(tenant): tenant for tenant in current_ckan_tenants}
+
+    tenant_ids = {
+        tenant_id: tenant
+        for tenant_id, tenant in tenant_ids.items()
+        if tenant_id in current_ckan_tenants_id
+    }
+
+    current_permissions = _permission_org_user({'id': user_dict['sub']})
+    if current_permissions:
+        permissions_dict = {permission['id']: permission['capacity'] for permission in current_permissions}
+    else:
+        permissions_dict = {}
+
+    for tenant_id, tenant_name in tenant_ids.items():
+        max_permission = get_highest_permission(user_dict['groups'], tenant_name)
+
+        tenant_permission = permissions_dict.get(tenant_id, 0)
+
+        if tenant_permission != max_permission:
+            _org_member_create({
+                "id": tenant_id,
+                "username": user_dict['sub'],
+                "role": max_permission
+            })
+
+    tenants_to_remove = [tenant_id for tenant_id in permissions_dict if tenant_id not in tenant_ids]
+
+    for tenant_id in tenants_to_remove:
+        tenant_name = permissions_dict.get(tenant_id, {}).get('name', 'Unbekannt')
+        print(f"Berechtigung für {tenant_name} ({tenant_id}) wird entfernt, da der Benutzer nicht mehr in den Gruppen vorhanden ist.")
+        _org_member_delete({
+            "id": tenant_id,
+            "user": user_dict['sub']
+        })
+
+    print("\nFinale Berechtigungen:")
+    for tenant_id, capacity in permissions_dict.items():
+        print(f"Tenant {tenant_id}: Berechtigung {capacity}")
+
+
+
+def get_highest_permission(user_groups, tenant_prefix):
+    tenant_groups = [group for group in user_groups if group.startswith(tenant_prefix)]
+
+    if not tenant_groups:
+        return None
+
+    highest_group = max(
+        tenant_groups,
+        key=lambda group: group_hierarchy.get(group.split('.')[1].replace('ckan-', ''), 0)
+    )
+
+    highest_group_name = highest_group.split('.')[1].replace('ckan-', '')
+
+    return highest_group_name
 
 def _create_user(userinfo):
     context = {
@@ -70,6 +147,16 @@ def _create_user(userinfo):
     
     return _get_user_by_email(created_user_dict['email'])
 
+
+def _group_member_create(userinfo):
+    context = {
+        u'ignore_auth': True,
+    }
+    response = tk.get_action(
+        u'group_member_create'
+    )(context, userinfo)
+
+    return response
 
 def button_style():
 
